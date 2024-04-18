afde28b40cff889a864fb945c635587e002b891a
Hashiq-M <mhashiq110@gmail.com>
2024-04-18 16:30:10 +0530

    portfolio

--- app/page.js
+++ app/page.js
@@ -1,113 +1,124 @@
-import Image from 'next/image'
-
+import Link from "next/link";
+import contact from "./contact/page";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main>
+      <div className="bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen font-sans text-white mx-auto">
+        <div className=" max-w-screen-xl mx-auto px-4 py-10">
+          <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio</h1>
+          <div className="lg:float-right  mx-auto">
+            <p className=" text-4xl font-bold pr-2 mx-auto">
+              Wanna Contact Me 📞
+            </p>
+            <img
+              className=" size-1/2 hidden lg:block"
+              src="https://media4.giphy.com/media/hru8na0gG8BoR2rK3l/giphy.gif?cid=6c09b9529u61j4o4n82y4pjl9iphmltb9197ujimg4hshaft&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s"
             />
-          </a>
+            <Link href="/contact">
+              <button className="text-xl font-bold border animate-bounce mt-6 rounded-lg p-2">
+                Click Me😁
+              </button>
+            </Link>
+          </div>
+          <p className="text-2xl pt-5 font-bold pb-4">
+            I'm ASHIK ✌️, a passionate developer 🧑🏼‍💻.
+          </p>
+
+          <p className="text-2xl mt-5 font-bold">The skills I Bring! 💻</p>
+
+          <div className="mb-8 mt-5">
+            <h2 className="text-xl font-semibold mb-2">
+              Web Development Skills
+            </h2>
+            <ul className="list-inside ml-4 sm:flex">
+              <li>React Js | </li>
+              <li>&nbsp;Next Js | </li>
+              <li>&nbsp;Tailwind CSS | </li>
+              <li>&nbsp;Bootstrap | </li>
+              <li>&nbsp;HTML | </li>
+              <li>&nbsp;CSS | </li>
+              <li>&nbsp;Javascript</li>
+            </ul>
+          </div>
+
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">
+              Programming Languages
+            </h2>
+            <ul className="list-inside ml-4 sm:flex">
+              <li>Java | </li>
+              <li>&nbsp;Python</li>
+            </ul>
+          </div>
+
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">Designing Tools</h2>
+            <ul className="list-inside ml-4 sm:flex">
+              <li>Figma | </li>
+              <li>&nbsp;Canva</li>
+            </ul>
+          </div>
+
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">Database</h2>
+            <ul className="list-inside ml-4 sm:flex">
+              <li>MySQL</li>
+            </ul>
+          </div>
+
+          <p className="text-2xl mt-5 font-bold">Projects I've Worked On! 🚀</p>
+
+          <div className="mb-8 mt-5 max-w-screen-xl mx-auto">
+            <h2 className="text-xl font-semibold mb-2">
+              At-Risk Student Prediction
+            </h2>
+            <ul className="ml-8 list-disc  leading-loose">
+              <li>
+                It predicts their performance based on their percentage and
+                their presence in the class.
+              </li>
+              <li>
+                This was done by using HTML and CSS for frontend and Python for
+                backend.
+              </li>
+              <li>
+                For training data, used the teachable machine which gives
+                accuracy more than 75% if trained perfectly.
+              </li>
+            </ul>
+          </div>
+
+          <div className="mb-8 mt-5 max-w-screen-xl mx-auto">
+            <h2 className="text-xl font-semibold mb-2">
+              Forest Fire Alarm System using Arduino and GSM Module
+            </h2>
+            <ul className="ml-8 list-disc leading-loose">
+              <li>
+                It is an IoT-based project used for detecting forest fire from a
+                long distance.
+              </li>
+              <li>
+                To alert the forest department to identify the forest fire
+                without risking their lives.
+              </li>
+            </ul>
+          </div>
+
+          <div className="mb-8 mt-5 max-w-screen-xl mx-auto">
+            <h2 className="text-xl font-semibold mb-2">
+              A Website for Buying AAA Games (Front End)
+            </h2>
+            <ul className="ml-8 list-disc leading-loose">
+              <li>It is a front-end website for buying PC games.</li>
+              <li>
+                Inspired by Epic Games and a designing project done by me on
+                Figma.
+              </li>
+              <li>This was done using HTML, CSS, and Bootstrap.</li>
+            </ul>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
